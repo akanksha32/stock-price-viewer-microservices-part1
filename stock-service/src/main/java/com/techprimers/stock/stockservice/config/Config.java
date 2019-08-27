de6a9ee87fd7c423b6b3aaa1f24e3ef5e2c61302
Akanksha Aggarwal <aggarwal.akanksha93@gmail.com>
2019-08-28 02:24:58 +0530

    Update Config.java

--- stock-service/src/main/java/com/techprimers/stock/stockservice/config/Config.java
+++ stock-service/src/main/java/com/techprimers/stock/stockservice/config/Config.java
@@ -7,6 +7,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class Config {
 
+    @LoadBalanced
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
